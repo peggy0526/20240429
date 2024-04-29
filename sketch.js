fe63514a4524402c0118f59b7c7088d380a19abf
peggy0526 <150638498+peggy0526@users.noreply.github.com>
2024-04-29 10:29:12 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,30 @@
+var captureGraphics
+var capture_width = 640
+var capture_height = 480
+var span = 5
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO) //啟動攝影機
-  capture.size(320,240);//設定顯示畫面大小
-
+  capture.size(capture_width,capture_height);//設定顯示畫面大小
+  captureGraphics = captureGraphics(capture_width,capture_height)
+  captureGraphics.translate(capture_width,0)
+  captureGraphics.scale(-1,1)
+  
 }
 
 function draw() {
   background(220);
+  noStroke()
   push()
-  translate(width/2-160,height/2-120)
-   image(capture,0, 0)
+  translate(width/2-capture_width,height/2-capture_height)
+   captureGraphics.image(capture,0, 0)  //在(0,0)
+   for(var x = 0; x< captureGraphic.width;x=x+span ){
+    for(var y = 0; y< captureGraphic.height;y=y+span ){
+      var pixel = captureGraphics.get(x,y)
+      fill(pixel)
+      rect(x,y,span)
+    }
+   }
   pop()
 }
-
