dff477ababad1cd3399e60855dfe5db3f624dfe9
peggy0526 <150638498+peggy0526@users.noreply.github.com>
2024-04-29 09:51:01 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  capture = createCapture(VIDEO) //啟動攝影機
+  capture.size(320,240);//設定顯示畫面大小
+
 }
 
 function draw() {
   background(220);
+  push()
+  translate(width/2-160,height/2-120)
+   image(capture,0, 0)
+  pop()
 }
+
